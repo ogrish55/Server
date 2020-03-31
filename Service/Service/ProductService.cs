2a78b8c0acecf53e676cbed4cf0b3839141c436d
Edin Mesanovic <1078090@ucn.dk>
2020-03-31 14:09:55 +0200

    Tilføjelse af services

--- Service/Service/ProductService.cs
+++ Service/Service/ProductService.cs
@@ -8,7 +8,7 @@ using System.Text;
 namespace Service
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class Service1 : IService1
+    public class Service1 : IProductService
     {
         public string GetData(int value)
         {
