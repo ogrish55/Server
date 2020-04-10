e539f18efb98f21814f83335a989534d77eba2e9
Dan Lund Pedersen <danlund1994@gmail.com>
2020-04-10 10:47:19 +0200

    Tilføjet klasser i Control, Data, Model, Service og ServerTest for DeliveryDescription

---
+++ Service/Control/DeliveryDescriptionControl.cs
@@ -0,0 +1,40 @@
+﻿using Service.Data;
+using Service.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Control
+{
+    public class DeliveryDescriptionControl
+    {
+        IDataDeliveryDescription dataDeliveryDescription;
+        public DeliveryDescriptionControl()
+        {
+            dataDeliveryDescription = new DataDeliveryDescription();
+        }
+
+        public void InsertDeliveryDescription(ServiceDeliveryDescription deliveryDescription)
+        {
+            dataDeliveryDescription.InsertDeliveryDescription(deliveryDescription);
+        }
+
+        public ServiceDeliveryDescription GetDeliveryDescriptionById(int deliveryId)
+        {
+            ServiceDeliveryDescription deliveryDescription = dataDeliveryDescription.GetDeliveryDescriptionById(deliveryId);
+            return deliveryDescription;
+        }
+
+        public void UpdateDeliveryDescription(ServiceDeliveryDescription deliveryDescription)
+        {
+            dataDeliveryDescription.UpdateDeliveryDescription(deliveryDescription);
+        }
+
+        public void DeleteDeliveryDescription(int deliveryId)
+        {
+            dataDeliveryDescription.DeleteDeliveryDescription(deliveryId);
+        }
+    }
+}
---
+++ Service/Data/IDataDeliveryDescription.cs
@@ -0,0 +1,17 @@
+﻿using Service.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Data
+{
+    public interface IDataDeliveryDescription
+    {
+        void InsertDeliveryDescription(ServiceDeliveryDescription deliveryDescription);
+        ServiceDeliveryDescription GetDeliveryDescriptionById(int deliveryId);
+        int UpdateDeliveryDescription(ServiceDeliveryDescription deliveryDescription);
+        int DeleteDeliveryDescription(int deliveryId);
+    }
+}
