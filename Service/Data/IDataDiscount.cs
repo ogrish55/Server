8408a7eae5f3f46c4ff910107775332595b63398
Lars Peter Gerald Sørensen <1068772@ucn.dk>
2020-04-12 21:21:20 +0200

    Discount tilføjet: Control Data Model Service

---
+++ Service/Data/IDataDiscount.cs
@@ -0,0 +1,15 @@
+﻿using Service.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Data
+{
+    interface IDataDiscount
+    {
+        void InsertDiscount(ServiceDiscount discount);
+        int DeleteDiscount(int discountId);
+    }
+}
---
+++ Service/Model/ServiceDiscount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Model
+{
+    [DataContract]
+    class ServiceDiscount
+    {
+        [DataMember]
+        public int DiscountId { get; set; }
+
+        [DataMember]
+        public string DiscountCode { get; set; }
+
+        [DataMember]
+        public decimal DiscountAmount { get; set; }
+    }
+}
