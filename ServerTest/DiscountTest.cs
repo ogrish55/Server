cb75f5309c3600e3aed419bf1417f46ba8315784
Shurikanz <1078121@gmail.com>
2020-04-27 12:05:26 +0200

    DiscountByCodeTest

---
+++ ServerTest/DiscountTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Service.Data;
+
+namespace ServiceTest
+{
+    [TestClass]
+    public class DiscountTest
+    {
+        [TestMethod]
+        public void GetDiscountByCodeTest()
+        {
+            // Arrange
+            DataDiscount dataDiscount = new DataDiscount();
+
+
+            // Act
+            int Amount = dataDiscount.GetDiscountByCode("acksyp");
+
+            // Assert
+            Assert.AreEqual(10, Amount);
+        }
+    }
+}
