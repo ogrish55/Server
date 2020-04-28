2c0d0156f923b2410629d8880eb09ef08fe696d0
Kristoffer K <Kristoffer92k@hotmail.com>
2020-04-28 13:59:05 +0200

    forsøgt at implementere optimistisk låsning i DataProduct ifbm opdatering af produktet.

--- Service/Data/DataProduct.cs
+++ Service/Data/DataProduct.cs
@@ -72,7 +72,7 @@ namespace Service.Data
                 using (SqlCommand cmdGetProductById = connection.CreateCommand())
                 {
                     ServiceProduct product = new ServiceProduct();
-                    cmdGetProductById.CommandText = "SELECT productId, name, price, description, amountOnStock FROM Product WHERE productId = @productId";
+                    cmdGetProductById.CommandText = "SELECT productId, name, price, description, amountOnStock, brand FROM Product WHERE productId = @productId";
                     cmdGetProductById.Parameters.AddWithValue("productId", productId);
                     SqlDataReader productReader = cmdGetProductById.ExecuteReader();
 
@@ -98,12 +98,20 @@ namespace Service.Data
                 connection.Open();
                 using (SqlCommand cmdInsertProduct = connection.CreateCommand())
                 {
-                    cmdInsertProduct.CommandText = "INSERT INTO Product (name, price, description, amountOnStock, brand) OUTPUT INSERTED.productId VALUES (@name, @price, @description, @amount, @brand)";
+                    cmdInsertProduct.CommandText = "INSERT INTO Product (name, price, description, amountOnStock, brand, category) OUTPUT INSERTED.productId VALUES (@name, @price, @description, @amount, @brand, @category)";
                     cmdInsertProduct.Parameters.AddWithValue("name", product.Name);
                     cmdInsertProduct.Parameters.AddWithValue("price", product.Price);
                     cmdInsertProduct.Parameters.AddWithValue("description", product.Description);
                     cmdInsertProduct.Parameters.AddWithValue("amount", product.AmountOnStock);
-                    cmdInsertProduct.Parameters.AddWithValue("brand", product.Brand);
+                    if (product.Brand == null)
+                    {
+                        cmdInsertProduct.Parameters.AddWithValue("brand", "NO_BRAND");
+                    }
+                    else
+                    {
+                        cmdInsertProduct.Parameters.AddWithValue("brand", product.Brand);
+                    }
+                    cmdInsertProduct.Parameters.AddWithValue("category", "NO_CATEGORY");
 
                     insertedId = (int)cmdInsertProduct.ExecuteScalar();
                 }
@@ -113,21 +121,43 @@ namespace Service.Data
 
         public int UpdateProduct(ServiceProduct product)
         {
-            int rowsAffected;
-            using (SqlConnection connection = new SqlConnection(_connectionString))
+            int rowsAffected = -1;
+            for (int i = 0; i < 5; i++)
             {
-                connection.Open();
-                using (SqlCommand cmdUpdateProduct = connection.CreateCommand())
+                using (SqlConnection connection = new SqlConnection(_connectionString))
                 {
-                    cmdUpdateProduct.CommandText = "UPDATE Product SET name = @name, price = @price, description = @description, brand = @brand WHERE productId = @productId";
-                    cmdUpdateProduct.Parameters.AddWithValue("name", product.Name);
-                    cmdUpdateProduct.Parameters.AddWithValue("price", product.Price);
-                    cmdUpdateProduct.Parameters.AddWithValue("description", product.Description);
-                    cmdUpdateProduct.Parameters.AddWithValue("productId", product.ProductId);
-                    cmdUpdateProduct.Parameters.AddWithValue("brand", product.Brand);
+                    connection.Open();
+                    using (SqlCommand cmdUpdateProduct = connection.CreateCommand())
+                    {
+                        byte[] rowId = null;
+                        cmdUpdateProduct.CommandText = "SELECT rowID FROM Product WHERE productId = @productId";
+                        cmdUpdateProduct.Parameters.AddWithValue("productId", product.ProductId);
+                        SqlDataReader reader = cmdUpdateProduct.ExecuteReader();
 
-                    rowsAffected = cmdUpdateProduct.ExecuteNonQuery();
+                        while (reader.Read())
+                        {
+                            rowId = (byte[])reader["rowId"];
+                        }
+                        reader.Close();
+
+                        cmdUpdateProduct.CommandText = "UPDATE Product SET name = @name, price = @price, description = @description, brand = @brand, amountOnStock = @amountOnStock WHERE productId = @productIdd AND rowID = @rowId";
+                        cmdUpdateProduct.Parameters.AddWithValue("name", product.Name);
+                        cmdUpdateProduct.Parameters.AddWithValue("price", product.Price);
+                        cmdUpdateProduct.Parameters.AddWithValue("description", product.Description);
+                        cmdUpdateProduct.Parameters.AddWithValue("productIdd", product.ProductId);
+                        cmdUpdateProduct.Parameters.AddWithValue("brand", product.Brand);
+                        cmdUpdateProduct.Parameters.AddWithValue("amountOnStock", product.AmountOnStock);
+                        cmdUpdateProduct.Parameters.AddWithValue("rowId", rowId);
+
+                        rowsAffected = cmdUpdateProduct.ExecuteNonQuery();
+
+                        if(rowsAffected != 0)
+                        {
+                            break;
+                        }
+                    }
                 }
+                
             }
             return rowsAffected;
         }
